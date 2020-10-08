3cad01f46411a8ded35238117f47553102501c8e
arinsingh7 <71252501+arinsingh7@users.noreply.github.com>
2020-10-08 18:28:05 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -51,7 +51,9 @@ function draw() {
   background(0);
 
   Engine.update(engine);
-
+  	
+  
+	
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
 
